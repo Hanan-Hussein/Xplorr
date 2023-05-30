8a2f673fb8b5ebca0d3075f4f8451fea1a893def
Hanan-Hussein <hanhuss035@gmail.com>
2023-05-30 12:11:07 +0300

    admin-added menu to admin

--- xplorr/admin.py
+++ xplorr/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
-
+from .models import Country, City, Goals, Profile, Accomodation, Sightseeing
 # Register your models here.
+
+class countryAdmin(admin.ModelAdmin):
+    list_display =("name", "iso", "code")
+    list_filter =("name","code")
+
+class cityAdmin(admin.ModelAdmin):
+    list_display=("name", "country")
+
+class goalsAdmin(admin.ModelAdmin):
+    list_display = ("goalName", "moneySaved", "user", "city")
+
+class profileAdmin(admin.ModelAdmin):
+    list_display=("user","profile_image")
+
+class accomodationAdmin(admin.ModelAdmin):
+    list_display=("name","location", "city", "price")
+    list_filter = ("city", "price")
+
+class sightSeeAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "city", "price")
+    list_filter =("price", "location")
+
+all = [(Country,countryAdmin),(City,cityAdmin),
+(Goals,goalsAdmin),(Profile,profileAdmin),(Accomodation,accomodationAdmin),(Sightseeing, sightSeeAdmin)]
+
+for i in all:
+    admin.site.register(i[0],i[1])
+# admin.site.register(Country, countryAdmin)
